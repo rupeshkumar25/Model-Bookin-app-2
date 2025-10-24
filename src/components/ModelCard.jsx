bded2a71188e9ab8a53e3ee12b565260570c101b
Rupesh kumar <53491959+rupeshkumar25@users.noreply.github.com>
2025-10-25 01:59:46 +0530

    Update ModelCard.jsx

--- src/components/ModelCard.jsx
+++ src/components/ModelCard.jsx
@@ -4,12 +4,17 @@ import { Link } from 'react-router-dom';
 export default function ModelCard({ model }) {
   return (
     <Link to={`/profile/${model.id}`}>
-      <div className="bg-white rounded-xl shadow-lg p-4 hover:scale-105 transform transition">
-        <img src={model.images[0]} alt={model.name} className="rounded-xl w-full h-48 object-cover" />
-        <h2 className="text-xl font-bold mt-2">{model.name}</h2>
-        <p className="text-gray-600">{model.age} years</p>
-        <p className="text-gray-500 text-sm mt-1">{model.description}</p>
-      </div>
-    </Link>
+  <div className="bg-white rounded-xl shadow-lg p-4 hover:scale-105 transform transition duration-300">
+    <img 
+      src={model.images[0]} 
+      alt={model.name} 
+      className="rounded-xl w-full h-48 sm:h-64 object-cover"
+    />
+    <h2 className="text-xl font-bold mt-2">{model.name}</h2>
+    <p className="text-gray-600">{model.age} years</p>
+    <p className="text-gray-500 text-sm mt-1">{model.description}</p>
+  </div>
+</Link>
+
   );
 }
